7a27129458e55dfd85e863db3c346b6d0eb98d32
pdnt <pdntspa@protonmail.ch>
2024-01-19 12:05:56 -0300

    Create NAT-Gateways module

--- main.tf
+++ main.tf
@@ -19,3 +19,17 @@ module "vpc" {
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
 
+# Create NAT-Gateways module
+module "nat_gateway" {
+  source                     = "git@github.com:pdnt/terraform-modules.git//nat-gateway"
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
