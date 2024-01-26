8e6eeee81e38cefc85d46b5a7107e930ad853bb7
pdnt <pdntspa@protonmail.ch>
2024-01-26 11:17:22 -0300

    Add Application Load Balancer (alb) module

--- main.tf
+++ main.tf
@@ -65,3 +65,16 @@ module "ssl_certificate" {
   domain_name       = var.domain_name
   alternative_names = var.alternative_names
 }
+
+# Create application load balancer
+module "application_load_balancer" {
+  source                = "git@github.com:pdnt/terraform-modules.git//alb"
+  project_name          = local.project_name
+  environment           = local.environment
+  alb_security_group_id = module.security_group.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  target_type           = var.target_type
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.ssl_certificate.certificate_arn
+}
