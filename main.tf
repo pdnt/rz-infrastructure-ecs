5ba89c215f4924dd9b673ada29806f2dcaec1ed1
pdnt <pdntspa@protonmail.ch>
2024-01-29 20:47:10 -0300

    Add auto scaling group (asg) module

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "ecs_task_execution_role" {
   environment          = local.environment
 }
 
-# create ecs cluster, task definition and service
+# Create ecs cluster, task definition and service
 module "ecs" {
   source                       = "git@github.com:pdnt/terraform-modules.git//ecs"
   project_name                 = local.project_name
@@ -110,4 +110,12 @@ module "ecs" {
   private_app_subnet_az2_id    = module.vpc.private_app_subnet_az1_id
   app_server_security_group_id = module.security_group.app_server_security_group_id
   alb_target_group_arn         = module.application_load_balancer.alb_target_group_arn
+}
+
+# Create auto scaling group
+module "ecs_asg" {
+  source = "git@github.com:pdnt/terraform-modules.git//asg-ecs"
+  project_name = local.project_name
+  environment = local.environment
+  ecs_service = module.ecs.ecs_service
 }
